9c18b74c1ff1b68692032bef132bcc7c5d6b03ba
Gustav Frid <49785603+gustavfrid@users.noreply.github.com>
2023-04-06 15:16:35 +0200

    Update urls.js

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,3 +1,3 @@
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+export const API_URL = 'https://happythoughts-b2xutvvxyq-lz.a.run.app/thoughts'
 export const LIKES_API_URL = id =>
-  `https://happy-thoughts-technigo.herokuapp.com/thoughts/${id}/like`
+  `https://happythoughts-b2xutvvxyq-lz.a.run.app/thoughts/${id}/like`
